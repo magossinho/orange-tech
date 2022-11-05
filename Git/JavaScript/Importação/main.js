ea5d31e889f20e61d0e4f79f5471f5a80fe8802a
magossinho <magossinho.ub@gmail.com>
2022-11-04 21:23:23 -0300

    Pokedex sem API

--- Git/JavaScript/Importação/main.js
+++ Git/JavaScript/Importação/main.js
@@ -2,4 +2,20 @@
 
 const {gets, print} = require ('./funcoes-auxiliares');
 
-print(gets());
+const numerosSorteado = [];
+
+for (let i = 0; i < 5; i++) {
+    const numerosSorteados = gets();
+    numerosSorteado.push(numerosSorteados);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteado.length; i++) {
+    const numerosSorteados = numerosSorteado[i];
+    if (numerosSorteados > maiorValor) {
+        maiorValor = numerosSorteados;
+    }
+    
+}
+print('O maior valor é: ' + maiorValor);
