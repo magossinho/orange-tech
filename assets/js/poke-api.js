6ba8f9ec9b90d9a04d3ea559b62b353beda3ce1b
LUIS ANTONIO MAGOSSO <94413025+magossinho@users.noreply.github.com>
2022-12-13 18:41:45 -0300

    Implementação da Api

---
+++ assets/js/poke-api.js
@@ -0,0 +1,18 @@
+
+const pokeApi = {}
+pokeApi.getPokemonsDetail = (pokemon) =>{
+    return fetch(pokemon.url).then((response) =>response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 9) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then ((pokemons)=> pokemons.map(pokeApi.getPokemonsDetail))
+        .then ((detailRequests)=> Promise.all(detailRequests))
+        .then((pokemonsDetails)=> pokemonsDetails)
+
+       
+}
